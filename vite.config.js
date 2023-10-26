732d5cf08df00a0de1207e75a9fca52ca2d14fcd
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2023-10-26 19:32:11 +0100

    fix: Fix vercel environment var

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
+import { loadEnv } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config(); // load env vars from .env
+
+export default ({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
   return {
-    server: {
-      open: true,
-    },
-    build: {
-      outDir: "build",
-    },
     plugins: [react()],
+    define: {
+      "import.meta.env.VITE_VERCEL_ANALYTICS_ID": JSON.stringify(
+        env.VERCEL_ANALYTICS_ID
+      ),
+    },
   };
-});
+};
